3015493b2cba8c4945341e86d6366db9a34fd215
Ichbinositu <hesbon.ezekiel@student.moringaschool.com>
2024-01-17 21:05:53 +0300

    Done with tasklister mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_task_description.value) //target grabs a specific input new_todo and its value after submision
+    form.reset()
+})
 });
+
+function buildToDo (Todos){
+  console.log(Todos)
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  li.textContent = `${Todos} `
+  li.appendChild(btn)
+  document.querySelector('#list').appendChild(li)
+}
+
+function handleDelete (e){
+  e.target.parentNode.remove()
+  }
